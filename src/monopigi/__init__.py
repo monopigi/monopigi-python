72e65897e5731aba920f76e5b260c4e8011526aa
Mo <moritz@info.nl>
2026-04-01 16:09:49 +0200

    chore(sdk): sync __init__.__version__ to 0.1.14

--- src/monopigi/__init__.py
+++ src/monopigi/__init__.py
@@ -4,7 +4,7 @@ from monopigi.client import AsyncMonopigiClient, MonopigiClient
 from monopigi.exceptions import AuthError, MonopigiError, NotFoundError, RateLimitError, TierError
 from monopigi.models import Country, Document, OutputFormat, QuotaInfo, Source, SourceStatus, Tier
 
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 __all__ = [
     "AsyncMonopigiClient",
     "AuthError",
